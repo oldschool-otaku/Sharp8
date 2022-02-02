6fa2b3a66f535db9328822e47af4d42be1550fe8
denamo <artstarxd@outlook.com>
2022-02-02 16:51:12 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sharp8
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            
+            Console.WriteLine(Tasks.Compare(a, b));
+            Console.WriteLine(Tasks.Factorial(5));
+            Console.WriteLine(Tasks.Fibonacci(13));
+            Console.WriteLine(Tasks.Change(ref a, ref b));
+        }
+    }
+}
---
+++ Tasks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Sharp8
+{
+    public class Tasks
+    {
+        public static int Compare(int a, int b)
+        {
+            return a>b ? a : a<b ? b : 0;
+        }
+
+        public static int Change(ref int a, ref int b)
+        {
+            (b, a) = (a, b);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            return 0;
+        }
+
+        public static int Factorial(int number)
+        {
+            return number == 0 ? 1 : number * Factorial(number - 1);
+        }
+
+        public static int Fibonacci(int number)
+        {
+            return number switch
+            {
+                0 => 0,
+                1 => 1
+                ,
+                _ => Fibonacci(number - 1) + Fibonacci(number - 2)
+            };
+        }
+        
+    }
+}
